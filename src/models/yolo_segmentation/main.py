cc692a615b9b704a8d4f1a002330462626a8cade
Luc Beltran <luc.beltran@gadz.org>
2024-09-06 14:41:51 +0000

    Configuration pour le run du yolo segmentation et iter 0 avec 2 epochs

--- src/models/yolo_segmentation/main.py
+++ src/models/yolo_segmentation/main.py
@@ -1,8 +1,6 @@
-
 import torch
 from ultralytics import YOLO
 import os
-
 
 # Chemin vers le fichier de poids
 model_path = 'yolov8n.pt'
@@ -14,21 +12,7 @@ data_path = 'data.yaml'
 save_dir = 'runs/detect'
 
 # Nombre d'epochs
-epochs = 4
-
-
-
-
-# URL du fichier de poids YOLOv8n
-weights_url = 'https://github.com/ultralytics/yolov8/releases/download/v8.0.0/yolov8n.pt'
-
-# Vérification si le fichier de poids existe déjà
-if not os.path.exists(model_path):
-    print(f"Téléchargement de {model_path}...")
-    urllib.request.urlretrieve(weights_url, model_path)
-    print(f"Téléchargement terminé : {model_path}")
-
-
+epochs = 2
 
 model = YOLO(model_path)
 model.train(data=data_path, epochs=epochs, save_dir=save_dir)
