ebdcf210f229cad08bbd7c2300016bbfe0205366
Joffrey ELIS <github@jomojo.net>
2024-09-06 10:26:01 +0200

    Dockerfile OK

---
+++ src/appFlask/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='saison',
+    author='la team Avengers',
+    description='for later',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "Pillow==9.1.1"
+    ],
+)
---
+++ src/appFlask/src/app.py
@@ -0,0 +1,9 @@
+# Imports
+from time import sleep
+
+# Initialisation message
+print("Saison app has started")
+
+# Going to sleep for 1 hour
+sleep(3600)
+
