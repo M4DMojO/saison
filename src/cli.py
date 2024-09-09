cb1990bc0f4153c8272c9107f5182d2233474216
Eric Hu <hueric1997@gmail.com>
2024-09-09 18:22:24 +0200

    adding cli to upload to bucket

--- src/cli.py
+++ src/cli.py
@@ -3,6 +3,25 @@ import os
 from src.models.finetuning import make_train_val_folder, make_generator, fit_and_export
 from src.data_cleaner import data_vgg_cls, data_seg, data_total, data_vgg_seg
 
+from google.cloud import storage
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+    storage_client = storage.Client().from_service_account_json('.credentials/keys.json')
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    generation_match_precondition = 0
+    blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
+    print(
+        f"File {source_file_name} uploaded to {destination_blob_name}."
+    )
 
 @click.group(name='"saison')
 def saison():
@@ -98,3 +117,17 @@ def finetuning(type_finetuning:str='big',
                    checkpoint_dir=chekcpoint_dir, 
                    from_pretrained=from_checkpoint,
                    epochs=epochs, from_epoch=from_epoch)
+    
+@saison.command("upload", help='upload a file to bucket')
+@click.option("--weights", 
+              "-w")
+def upload(weights:str):
+    if weights == "cls":
+        source = os.path.join("src", "models", "vgg_classification", "big", "checkpoint"," vgg16-0031.weights.h5")
+        dest = "vgg_classification.weights.h5"
+    elif weights == "seg":
+         source = os.path.join("src", "models", "yolo_segmentation", "model")
+         dest = "vgg_segmentation.weights.h5"
+    upload_blob("all-weights", 
+                source_file_name=source, 
+                destination_blob_name=dest)
