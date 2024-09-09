5538b16a945ad8508f28b8d78ddcbbe8521a9ca3
Luc Beltran <luc.beltran@gadz.org>
2024-09-09 07:18:57 +0000

    YOLO segmentation iter 1 result

--- src/models/yolo_segmentation/main.py
+++ src/models/yolo_segmentation/main.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 import os
 
 # Chemin vers le fichier de poids
-model_path = 'yolov8n.pt'
+model_path = 'runs/detect_iter0/train4/weights/best.pt'
 
 # Chemin vers le fichier de données
 data_path = 'data.yaml'
@@ -12,7 +12,7 @@ data_path = 'data.yaml'
 save_dir = 'runs/detect'
 
 # Nombre d'epochs
-epochs = 2
+epochs = 350
 
 model = YOLO(model_path)
 model.train(data=data_path, epochs=epochs, save_dir=save_dir)
