61c2cd5de3c3706f33680ee9fbf9563da8af5f96
Eric Hu <hueric1997@gmail.com>
2024-09-12 11:16:55 +0200

    fix vgg small name

--- src/appFlask/src/custom_function.py
+++ src/appFlask/src/custom_function.py
@@ -31,12 +31,14 @@ def get_weights_from_bucket(model:str):
         else:
             name = "yolo_total.pt"
         blob = bucket.blob(name)
+        print("loading :", name)
         destination_file_name = join(base_path, name)
         blob.download_to_filename(destination_file_name)
     elif model == "seg":
         for name in ["vgg_classification_small.keras", "yolo_segmentation.pt"]:
             blob = bucket.blob(name)
             destination_file_name = join(base_path, name)
+            print("loading :", name)
             blob.download_to_filename(destination_file_name)
     else:
         raise Exception("No such argument, use : cls|total|seg")
